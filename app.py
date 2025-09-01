3aac97d4d9f21c2e2caefb72021ab787bcb7ea8b
Chopkeeper <73922797+Chopkeeper@users.noreply.github.com>
2025-09-01 15:20:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -117,5 +117,8 @@ def report_stock():
     products=Product.query.order_by(Product.name).all()
     return render_template('report_stock.html', products=products, active_page='report_stock')
 
-if __name__=='__main__':
-    app.run(debug=True)
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
+
